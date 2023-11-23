674c89fef27ca398b9902e2a1ef354c032182eaa
San4eeez <AlexandroHell@yandex.ru>
2023-11-23 16:20:19 +0300

    несколько

--- Forma/Window4.axaml.cs
+++ Forma/Window4.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Npgsql;
 
 namespace Forma;
@@ -18,23 +19,60 @@ public partial class Window4 : Window
     
     private void vas()
     {
-        using (var cmd = dataSource.CreateCommand($"SELECT email FROM users"))
+        using (var cmd = dataSource.CreateCommand($"SELECT id, email, is_admin FROM users"))
         {
             var reader = cmd.ExecuteReader();
+            
 
             while (reader.Read())
             {
-                string tapok = reader[0].ToString();
+                string id_tapok = reader[0].ToString();
+                string tapok = reader[1].ToString();
+                string is_admin_tapok = reader[2].ToString();
+
 
                 // Создаем новый TextBlock
+                var newTextBlockId = new TextBlock
+                {
+                    Text = id_tapok,
+                    Foreground = Brushes.Red,
+                    Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
+                };
+                
                 var newTextBlock = new TextBlock
                 {
                     Text = tapok,
+                    Foreground = Brushes.Green,
                     Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
+                };
+                
+                var newTextBlockAdm = new TextBlock
+                {
+                    Text = is_admin_tapok,
+                    Foreground = Brushes.Blue,
+                    Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
+                };
+                
+                var newBorder = new Border()
+                {
+                    Background = Brushes.Yellow,
+                    Width = 50,
+                    Height = 50,
+                    Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
+                };
+                
+                var newStack = new StackPanel()
+                {
+                    
                 };
 
                 // Добавляем новый TextBlock в ItemsControl
-                TextBlocksContainer.Items.Add(newTextBlock);
+                newStack.Children.Add(newBorder);
+                newStack.Children.Add(newTextBlockId);
+                newStack.Children.Add(newTextBlock);
+                newStack.Children.Add(newTextBlockAdm);
+
+                TextBlocksContainer.Items.Add(newStack);
             }
         }
     }
