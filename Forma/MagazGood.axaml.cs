48816c92159fe8f14e6910bbb9f56e6f0229e490
San4eeez <AlexandroHell@yandex.ru>
2023-11-27 14:36:54 +0300

    Открытие страницы товара

---
+++ Forma/MagazGood.axaml.cs
@@ -0,0 +1,102 @@
+using System;
+using System.IO;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Layout;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Npgsql;
+
+
+namespace Forma;
+
+public partial class MagazGood : Window
+{
+    NpgsqlDataSource dataSource;
+    public MagazGood(string tid)
+    {
+        int id = Convert.ToInt32(tid);
+        var connectionString = "Host=localhost;Username=postgres;Password=admin;Database=forma";
+        dataSource = NpgsqlDataSource.Create(connectionString);
+        InitializeComponent();
+        vas(id);
+        
+    }
+    
+     private void vas(int id)
+    {
+        using (var cmd = dataSource.CreateCommand($"SELECT name,forlink,price from magaz where id = {id}"))
+        {
+            var reader = cmd.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                string id_tapok = reader[0].ToString();
+                string tapok = reader[1].ToString();
+                string tapok_price = reader[2].ToString();
+                
+                // Создаем новый TextBlock
+                var newTextBlockId = new TextBlock
+                {
+                    Text = id_tapok,
+                    Foreground = Brushes.Red,
+                    Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
+                };
+                
+                var newTextBlock = new TextBlock
+                {
+                    Text = tapok,
+                    Foreground = Brushes.Green,
+                    Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
+                };
+                var newTextBlockPrice = new TextBlock
+                {
+                    Text = tapok_price
+                };
+                
+                
+                
+                var newBorder = new Border()
+                {
+                    Background = Brushes.Yellow,
+                    Width = 50,
+                    Height = 50,
+                    Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
+                };
+                var newImg = new Image()
+                {
+                    Source = ImageHelper.LoadFromResource(new Uri($"avares://Forma/Assets/{tapok}"))
+
+                };
+
+                var newStack = new StackPanel()
+                {
+                    
+                };
+                // Добавляем новый TextBlock в ItemsControl
+                newStack.Children.Add(newBorder);
+                newStack.Children.Add(newTextBlockId);
+                newStack.Children.Add(newTextBlock);
+                newStack.Children.Add(newTextBlockPrice);
+                newStack.Children.Add(newImg);
+                TextBlocksContainer.Items.Add(newStack);
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- Forma/Window4.axaml.cs
+++ Forma/Window4.axaml.cs
@@ -24,76 +24,75 @@ public partial class Window4 : Window
     
     private void vas()
     {
-        using (var cmd = dataSource.CreateCommand($"SELECT name,forlink,price from magaz"))
+        using (var cmd = dataSource.CreateCommand($"SELECT name,forlink,price, id from magaz"))
         {
             var reader = cmd.ExecuteReader();
-            
+
             while (reader.Read())
             {
                 string id_tapok = reader[0].ToString();
                 string tapok = reader[1].ToString();
                 string tapok_price = reader[2].ToString();
-                
+                string id = reader[3].ToString();
 
-
-                // Создаем новый TextBlock
                 var newTextBlockId = new TextBlock
                 {
                     Text = id_tapok,
                     Foreground = Brushes.Red,
                     HorizontalAlignment = HorizontalAlignment.Center,
-
-                    Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
+                    Margin = new Thickness(5)
                 };
-                
-                
 
                 var newTextBlockPrice = new TextBlock
                 {
                     Text = tapok_price,
                     HorizontalAlignment = HorizontalAlignment.Center
-
                 };
-                
-                
-                
-                /*var newBorder = new Border()
-                {
-                    Background = Brushes.Yellow,
-                    Width = 50,
-                    Height = 50,
-                    Margin = new Thickness(5) // Можно настроить отступы по вашему желанию
-                };*/
 
                 var newImg = new Image()
                 {
                     Source = ImageHelper.LoadFromResource(new Uri($"avares://Forma/Assets/{tapok}")),
                     Width = 100,
                     Height = 100
-                    
                 };
-                
-                var newStack = new StackPanel()
+
+                var newBorder = new Border()
                 {
-                    Height = 100,
-                    Width = 100
                     
-                    
+                    BorderBrush = Brushes.Black,
+                    BorderThickness = new Thickness(1),
+                    Margin = new Thickness(5),
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    VerticalAlignment = VerticalAlignment.Stretch
                 };
 
-                // Добавляем новый TextBlock в ItemsControl
-                /*
-                newStack.Children.Add(newBorder);
-                */
-                newStack.Children.Add(newImg);
-                newStack.Children.Add(newTextBlockId);
-                newStack.Children.Add(newTextBlockPrice);
+                var newButton = new Button()
+                {
+                    Content = "Купить",
+                    Foreground = Brushes.White,
+                    Background = Brushes.Green,                    
+                };
 
-                TextBlocksContainer.Items.Add(newStack);
+                newButton.Click += (sender, args) => new MagazGood(id).Show(); 
+
+                // Добавляем новый TextBlock, Image в Border
+                newBorder.Child = new StackPanel()
+                {
+                    Children =
+                    {
+                        newImg,
+                        newTextBlockId,
+                        newTextBlockPrice,
+                        newButton
+                    }
+                };
+
+                // Добавляем Border в ItemsControl
+                TextBlocksContainer.Items.Add(newBorder);
             }
         }
     }
 
-
+   
     
 }
